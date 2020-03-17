f5a7290cb6aa1d7ab1f09cff3c5e4ed2119e38e8
AymericLeFeyer <lefeyer.aymeric@gmail.com>
2020-03-17 11:57:18 +0100

    Exercice 3

--- TP1/ex3_client.c
+++ TP1/ex3_client.c
@@ -6,33 +6,71 @@
 #include <sys/wait.h>
 #include <sys/msg.h>
 #include <string.h>
+#include <sys/ipc.h>
 
 #define MAX 64
 
-int main(int argc, char** argv) {
+// structure for message queue 
+struct mesg_buffer { 
+    long mesg_type; 
+    char mesg_text[100]; 
+} message; 
+  
+int main(int argc, char ** argv) 
+{ 
     key_t key; 
     int msgid; 
-    char message[MAX];
-    strcpy(message, "");
-    strcat(message, "5");
-    strcat(message, ",");
-    strcat(message, "8");
-  
-    // ftok to generate unique key 
-    key = ftok("progfile", 65); 
-  
-    // msgget creates a message queue 
-    // and returns identifier 
-    msgid = msgget(key, 0666 | IPC_CREAT); 
-  
-    printf("Write Data : "); 
+    
 
-  
-    // msgsnd to send message 
-    msgsnd(msgid, &message, sizeof(message), 0); 
-  
-    // display the message 
-    printf("Data send is : %s \n", message); 
+    if (fork() == 0) {
+
+        
+        // Code du pere
+        // ftok to generate unique key 
+        key = ftok("progfile", 65); 
+    
+        // msgget creates a message queue 
+        // and returns identifier 
+        msgid = msgget(key, 0666 | IPC_CREAT); 
+        msgrcv(msgid, &message, sizeof(message), 1, 0); 
+        printf("Data Received by client is : %s \n",  
+                    message.mesg_text); 
+
+
+        exit(0);
+        
+    }
+    else {
+        
+
+        // Code du fils
+        strcpy(message.mesg_text, "8"); 
+        strcat(message.mesg_text, ",");
+        strcat(message.mesg_text, "2");
+    
+        // ftok to generate unique key 
+        key = ftok("progfile", 65); 
+    
+        // msgget creates a message queue 
+        // and returns identifier 
+        msgid = msgget(key, 0666 | IPC_CREAT); 
+        message.mesg_type = 1; 
+        
+    
+        // msgsnd to send message 
+        msgsnd(msgid, &message, sizeof(message), 0); 
+        
+    
+        // display the message 
+        printf("Data send by client is : %s \n", message.mesg_text); 
+
+        printf("Appel du server\n");
+        execv("/home/aymeric/Git/TP_ProgSysteme/TP1/server", argv);
+       
+        exit(0);
+       
+    }
+       
   
     return 0; 
-}
+} 
--- TP1/ex3_server.c
+++ TP1/ex3_server.c
@@ -6,32 +6,70 @@
 #include <sys/wait.h>
 #include <sys/msg.h>
 #include <string.h>
+#include <sys/ipc.h> 
 
 #define MAX 64
 
-int main(int argc, char** argv) {
+// structure for message queue 
+struct mesg_buffer { 
+    long mesg_type; 
+    char mesg_text[100]; 
+} message; 
+  
+int main() 
+{ 
     key_t key; 
     int msgid; 
-    char message[MAX];
+    
+    
+    if (fork() == 0) {
+        // Code du fils
+        int s = 0;
+
+        char delim[] = ",";
+
+        
+    
+        // ftok to generate unique key 
+        key = ftok("progfile", 65); 
+    
+        // msgget creates a message queue 
+        // and returns identifier 
+        msgid = msgget(key, 0666 | IPC_CREAT); 
+    
+        // msgrcv to receive message 
+        msgrcv(msgid, &message, sizeof(message), 1, 0); 
+    
+        // display the message 
+        printf("Data Received by server is : %s \n",  
+                        message.mesg_text); 
+
+        char *ptr = strtok(message.mesg_text, delim);
+
+        while(ptr != NULL)
+        {
+            s += atoi(ptr);
+            ptr = strtok(NULL, delim);
+        }
+
+        sprintf(message.mesg_text, "%d", s);
+        msgsnd(msgid, &message, sizeof(message), 0); 
+
+        
+        exit(0);
+    }
+    else {
+        // Code du pere
+        
+       
     
 
-    // ftok to generate unique key 
-    key = ftok("progfile", 65); 
-  
-    // msgget creates a message queue 
-    // and returns identifier 
-    msgid = msgget(key, 0666 | IPC_CREAT); 
-  
-    // msgrcv to receive message 
-    msgrcv(msgid, &message, sizeof(message), 1, 0); 
-  
-    // display the message 
-    printf("Data Received is : %s \n",  
-                    message); 
+    
+    }
+    
   
     // to destroy the message queue 
     msgctl(msgid, IPC_RMID, NULL); 
   
     return 0; 
-    return EXIT_SUCCESS;
-}
+} 
