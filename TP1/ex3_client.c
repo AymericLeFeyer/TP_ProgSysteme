d4caab2d17eaff345fcc0c30d67152840c40f6a4
AymericLeFeyer <lefeyer.aymeric@gmail.com>
2020-03-17 10:13:16 +0100

    Ajout du sujet et debut ex3

--- TP1/ex3_client.c
+++ TP1/ex3_client.c
@@ -7,6 +7,32 @@
 #include <sys/msg.h>
 #include <string.h>
 
+#define MAX 64
+
 int main(int argc, char** argv) {
-    return EXIT_SUCCESS;
+    key_t key; 
+    int msgid; 
+    char message[MAX];
+    strcpy(message, "");
+    strcat(message, "5");
+    strcat(message, ",");
+    strcat(message, "8");
+  
+    // ftok to generate unique key 
+    key = ftok("progfile", 65); 
+  
+    // msgget creates a message queue 
+    // and returns identifier 
+    msgid = msgget(key, 0666 | IPC_CREAT); 
+  
+    printf("Write Data : "); 
+
+  
+    // msgsnd to send message 
+    msgsnd(msgid, &message, sizeof(message), 0); 
+  
+    // display the message 
+    printf("Data send is : %s \n", message); 
+  
+    return 0; 
 }
--- TP1/ex3_server.c
+++ TP1/ex3_server.c
@@ -7,6 +7,31 @@
 #include <sys/msg.h>
 #include <string.h>
 
+#define MAX 64
+
 int main(int argc, char** argv) {
+    key_t key; 
+    int msgid; 
+    char message[MAX];
+    
+
+    // ftok to generate unique key 
+    key = ftok("progfile", 65); 
+  
+    // msgget creates a message queue 
+    // and returns identifier 
+    msgid = msgget(key, 0666 | IPC_CREAT); 
+  
+    // msgrcv to receive message 
+    msgrcv(msgid, &message, sizeof(message), 1, 0); 
+  
+    // display the message 
+    printf("Data Received is : %s \n",  
+                    message); 
+  
+    // to destroy the message queue 
+    msgctl(msgid, IPC_RMID, NULL); 
+  
+    return 0; 
     return EXIT_SUCCESS;
 }
